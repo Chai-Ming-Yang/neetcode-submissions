dadd04ae57abcfcdde4604826d47986e4675435b
lone <tsuyoku2024@gmail.com>
2026-05-18 21:06:53 +0800

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        mn = intervals[:]
+        heapq.heapify(mn)
+        
+        res = [heapq.heappop(mn)]
+        while mn:
+            if res[-1][1] < mn[0][0]:
+                res.append(heapq.heappop(mn)); continue
+            res[-1][1] = max(res[-1][1], heapq.heappop(mn)[1])
+        return res + mn
